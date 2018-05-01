0adaf3e3c529825896d6faf43a86e50f0d732081
Himalee <himalee.tailor@gmail.com>
2018-05-01 12:33:43 +0100

    Added loop t retirement calculator

--- retirement_calculator.rb
+++ retirement_calculator.rb
@@ -1,22 +1,26 @@
 class RetirementCalculator
 
 def prompt_for_ages
-  puts "What is your current age?"
-  @current = gets.chomp
-  puts "At what age would you like to retire?"
-  @age = gets.chomp
-  retirement_output
+  loop do
+    puts "What is your current age?"
+    @current = gets.chomp
+    break if @current == "quit"
+    puts "At what age would you like to retire?"
+    @age = gets.chomp
+    retirement_output
+    break if @age == "quit"
+  end
 end
 
 def retirement_output
-  @years_until_retirement = @age.to_i - @current.to_i
-  @current_year = Time.new.year
-  @retirement_year = @current_year + @years.to_i
+  years_until_retirement = @age.to_i - @current.to_i
+  current_year = Time.new.year
+  retirement_year = current_year + years_until_retirement.to_i
   if @current.to_i > @age.to_i
-    puts "You retired #{@years_until_retirement.abs} years ago, nice!" # .abs turns negative into a positive
+    puts "You retired #{years_until_retirement.abs} years ago, nice!" # .abs turns negative into a positive
   else
-    puts "You have #{@years_until_retirement} years left until you can retire."
-    puts "It's #{@current_year}, so you can retire in #{@retirement_year}."
+    puts "You have #{years_until_retirement} years left until you can retire."
+    puts "It's #{current_year}, so you can retire in #{retirement_year}."
   end
 end
 
