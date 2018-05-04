659f9a5c062917d781da5547595d3bfe547af377
Himalee <himalee.tailor@gmail.com>
2018-05-04 11:46:45 +0100

    Displays output string

--- lib/counting_characters.rb
+++ lib/counting_characters.rb
@@ -1,5 +1,10 @@
 class CountingCharacters
 
+  def initialize(output = $stdout)
+    @output = output
+  end
+
+
   def counts_characters(words_to_count)
     @characters_count = words_to_count.size
     @words_to_count = words_to_count
@@ -9,6 +14,10 @@ class CountingCharacters
 
   def message
     "#{@words_to_count} has #{@characters_count} characters"
+  end
+
+  def display_prompt
+    @output.puts "Enter a word"
   end
 
 
--- spec/counting_characters_spec.rb
+++ spec/counting_characters_spec.rb
@@ -5,12 +5,24 @@ describe CountingCharacters do
   describe ".counts_characters" do
 
     context "given string, returns string and number of characters in string" do
-      it "hello returns ouput" do
+      it "hello returns message to user" do
         counting_characters = CountingCharacters.new
         expect(counting_characters.counts_characters("hello")).to eql("hello has 5 characters")
       end
     end
 
+  end
+
+  describe ".display_prompt" do
+
+    context "displays prompt to user" do
+      it "displays 'Enter a word'" do
+        output = StringIO.new # new instance of the class String IO, set it to the variable output
+        display = CountingCharacters.new(output) # new instance of the CountingCharacters class
+        display.display_prompt # method called display_greeting
+        expect(output.string).to eq("Enter a word\n") # calls method .string which reads strings
+      end
+    end
   end
 
 
